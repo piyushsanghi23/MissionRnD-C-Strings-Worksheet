ead78f8d27d0d77d065c3ff95af752507c70052b
piyushsanghi23 <piyushsanghi23@gmail.com>
2015-11-23 23:12:41 +0530

    Submit Commit

--- src/NumbertoStr.cpp
+++ src/NumbertoStr.cpp
@@ -18,8 +18,104 @@ NOTES: Don't create new string.
 */
 
 #include <stdio.h>
+int power(int a, int b){
+	int i = 0, sum = 1;
+	for (i = b; i > 0; i--)
+	{
+		sum = sum*a;
+	}
+	return sum;
+
+}
+void reverse(char *str, int len)
+{
+	int i = 0, j = len - 1, temp;
+	while (i<j)
+	{
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+		i++; j--;
+	}
+}
 
 
-void number_to_str(float number, char *str,int afterdecimal){
+int intToStr(int x, char str[], int d)
+{
+	int i = 0;
+	while (x)
+	{
+		str[i] = (x % 10) + '0';
+		x = x / 10;
+		i = i + 1;
+	}
+
+	
+	while (i < d)
+		str[i++] = '0';
+
+	reverse(str, i);
+	str[i] = '\0';
+	return i;
+}
+void update(char *str, int i)
+{
+	int len = i - 1;
+	int j = 0;
+	for (j = len; j >= 0; j--)
+	{
+		str[j + 1] = str[j];
+	}
+	str[0] = '-';
+}
+void number_to_str(float number, char *str,int afterdecimal)
+{
+	float n = number;
+	if (n<0)
+	{
+		n = -n;
+		
+		int ipart = (int)n;
+
+		
+		float fpart = n - (float)ipart;
+		printf("%d %f", ipart, fpart);
+		
+		int i = intToStr(ipart, str, 0);
+		printf(" \n %d", i);
+		update(str, i);
+		i = i + 1;
+	
+		if (afterdecimal != 0)
+		{
+			str[i] = '.';  
+
+		
+			fpart = fpart * power(10, afterdecimal);
+
+			intToStr((int)fpart, str + i + 1, afterdecimal);
+		}
+	}
+	else{
+		int ipart = (int)n;
+
+	
+		float fpart = n - (float)ipart;
+	
+		int i = intToStr(ipart, str, 0);
+		
+
+
+		
+		if (afterdecimal != 0)
+		{
+			str[i] = '.'; 
+			fpart = fpart * power(10, afterdecimal);
+
+			intToStr((int)fpart, str + i + 1, afterdecimal);
+		}
+	}
+
 	
 }
+
